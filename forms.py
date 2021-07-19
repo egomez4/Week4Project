fbedaa42bccba234d8ff30ab0aad0230f9ae2c04
Eduardo Gomez <egomez4@uncc.edu>
2021-07-19 22:11:50 +0000

    Added style guide checker and pages display registration forms

---
+++ forms.py
@@ -0,0 +1,72 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, SelectField
+from wtforms.validators import DataRequired, Length, Email, EqualTo
+
+states = [ ("AL", "Alabama"),
+          ("AK", "Alaska"),
+          ("AZ", "Arizona"),
+          ("AR", "Arkansas"),
+          ("CA", "California"),
+          ("CO", "Colorado"),
+          ("CT", "Connecticut"),
+          ("DE", "Delaware"),
+          ("DC", "District Of Columbia"),
+          ("FL", "Florida"),
+          ("GA", "Georgia"),
+          ("HI", "Hawaii"),
+          ("ID", "Idaho"),
+          ("IL", "Illinois"),
+          ("IN", "Indiana"),
+          ("IA", "Iowa"),
+          ("KS", "Kansas"),
+          ("KY", "Kentucky"),
+          ("LA", "Louisiana"),
+          ("ME", "Maine"),
+          ("MD", "Maryland"),
+          ("MA", "Massachusetts"),
+          ("MI", "Michigan"),
+          ("MN", "Minnesota"),
+          ("MS", "Mississippi"),
+          ("MO", "Missouri"),
+          ("MT", "Montana"),
+          ("NE", "Nebraska"),
+          ("NV", "Nevada"),
+          ("NH", "New Hampshire"),
+          ("NJ", "New Jersey"),
+          ("NM", "New Mexico"),
+          ("NY", "New York"),
+          ("NC", "North Carolina"),
+          ("ND", "North Dakota"),
+          ("OH", "Ohio"),
+          ("OK", "Oklahoma"),
+          ("OR", "Oregon"),
+          ("PA", "Pennsylvania"),
+          ("RI", "Rhode Island"),
+          ("SC", "South Carolina"),
+          ("SD", "South Dakota"),
+          ("TN", "Tennessee"),
+          ("TX", "Texas"),
+          ("UT", "Utah"),
+          ("VT", "Vermont"),
+          ("VA", "Virginia"),
+          ("WA", "Washington"),
+          ("WV", "West Virginia"),
+          ("WI", "Wisconsin"),
+          ("WY", "Wyoming")   
+]
+
+class AnimalsForm(FlaskForm):
+    #animalType = SelectField(
+    #                       validators=[DataRequired(), Length(min=2, max=20)])
+    #breed = StringField('Email',
+    #                    validators=[DataRequired(), Email()])
+    state = SelectField('State', choices = states, validators=[DataRequired()])
+    kids = BooleanField('Good with Kids', default=False)
+    dogs = BooleanField('Good with Dogs', default=False)
+    cats = BooleanField('Good with Cats', default=False)
+    submit = SubmitField('Search')
+    
+class OrganizationForm(FlaskForm):
+    zip_code = StringField('Zip Code')
+    state = SelectField('State', choices=states, validators=[DataRequired()])
+    submit = SubmitField('Search')
--- routes.py
+++ routes.py
@@ -1,27 +1,38 @@
 #sudo pip3 install petpy
 import petpy
 from flask import Flask, render_template, url_for, flash, redirect
+from forms import AnimalsForm, OrganizationForm
+from flask_behind_proxy import FlaskBehindProxy
+
 
 app = Flask(__name__)
+proxied = FlaskBehindProxy(app)
+app.config['SECRET_KEY'] = '9964c5b701ffc08bb7832c6c9d603d04'
 
 api_key = 'JNGvI97bit2E3e4Vawh0diK8VQyRwXvIoaK8sGAm91DevlHJwU'
 api_secret = 'IijpJC2ivqOzvZrJItbXxBNp7KUHZefKsP5YlNtY'
 
 pf = petpy.Petfinder(api_key, secret=api_secret)
 
-#print(pf.breeds())
+#print(pf.animal_types())
 
 @app.route('/')
 def home():
     return render_template('home.html', subtitle = 'Home')
 
-@app.route('/animals')
+@app.route('/animals', methods=['GET', 'POST'])
 def animals():
-    return render_template('animals.html', subtitle='Animals')
+    form = AnimalsForm()
+    if form.validate_on_submit():
+        return redirect(url_for('home'))
+    return render_template('animals.html', subtitle='Animals', form=form)
 
-@app.route('/organizations')
+@app.route('/organizations', methods=['GET', 'POST'])
 def organizations():
-    return render_template('organizations.html', subtitle='Organizations')
+    form = OrganizationForm()
+    zip_code = form.zip_code.data
+    print(zip_code)
+    return render_template('organizations.html', subtitle='Organizations', form=form)
 
 if __name__ == '__main__':               # this should always be at the end
     app.run(debug=True, host="0.0.0.0")
