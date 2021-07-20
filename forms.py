b88145b5673f6b4a86215e06bbd42f79e90a6ce2
Eduardo Gomez <egomez4@uncc.edu>
2021-07-20 15:58:03 +0000

    Pep8 style changes

--- forms.py
+++ forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, BooleanField, SelectField
+from wtforms import StringField, SubmitField, BooleanField, SelectField
 from wtforms.validators import DataRequired, Length, Email, EqualTo
 
 states = [("AL", "Alabama"),
