a19ab607e37b04971f9382bf0bf49b3b4569a9a1
Eduardo Gomez <egomez4@uncc.edu>
2021-07-21 16:09:05 +0000

    Organizations and Animals form completed and can get information displayed onto web page. Also determined how to render images on page and use a for loop to display results. Added basic testing of webpages.
    Co-authored-by: Araceli Corley <abcorley@bu.edu>
    Co-authored-by: Jordy Reyes <Jordyreyes8@gmail.com>

--- routes.py
+++ routes.py
@@ -1,5 +1,6 @@
 # sudo pip3 install petpy
 import petpy
+import pandas as pd
 from flask import Flask, render_template, url_for, flash, redirect
 from forms import AnimalsForm, OrganizationForm
 from flask_behind_proxy import FlaskBehindProxy
@@ -30,29 +31,58 @@ def animals():
         dogs = form.dogs.data
         cats = form.cats.data
         data = findAnimal(animalType, state, kids, dogs, cats)
+        dictionary = createDictionary(data)
+        df = dict_to_dataframes(dictionary)
         return render_template(
             'animalsResults.html',
             subtitle='results',
-            data=data)
+            data = df
+            )
     return render_template('animals.html', subtitle='Animals', form=form)
 
 
 @app.route('/organizations', methods=['GET', 'POST'])
 def organizations():
     form = OrganizationForm()
-    zip_code = form.zip_code.data
-    print(zip_code)
-    return render_template(
-        'organizations.html',
-        subtitle='Organizations',
-        form=form)
+
+    if form.validate_on_submit():
+        # zip_code = form.zip_code.data
+        state = form.state.data
+        zip_code = form.zip_code.data
+        data = find_organization(state=state, zip_code=zip_code)
+        img_url = data['organizations'][0]['photos'][0]['medium']
+        print(img_url)
+        #dictionary = createDictionary(data)
+        #df = dict_to_dataframes(dictionary)
+        return render_template(
+            'organizationResults.html',
+            subtitle='Organizations',
+            data=data, img_url=img_url)
+    return render_template('organizations.html', subtitle='Organizations', form=form)
+
 
 
 def findAnimal(animalType, state, kids, dogs, cats):
     return pf.animals(animal_type=animalType, location=state,
                       good_with_children=kids,
                       good_with_dogs=dogs,
-                      good_with_cats=cats, return_df=True)
+                      good_with_cats=cats)
+
+def find_organization(state, zip_code):
+    return pf.organizations(state=state, location=zip_code)
+
+def createDictionary(data):
+    animalsDict = {}
+    for animals in data['animals']:
+        animalsDict[animals['id']] = [animals['name'], animals['age'], animals['size'], animals['url']]
+    
+    return animalsDict
+
+def dict_to_dataframes(animalsDict):
+    df = pd.DataFrame.from_dict(animalsDict,
+                                  orient='index', columns=['Name','Age','Size', 'Link'])
+    return df
+
 
 
 if __name__ == '__main__':               # this should always be at the end
---
+++ tests/testPages.py
@@ -0,0 +1,29 @@
+import unittest, sys
+
+sys.path.append('../Week4Project') # imports python file from parent directory
+from main import app #imports flask app object
+
+class BasicTests(unittest.TestCase):
+
+    # executed prior to each test
+    def setUp(self):
+        self.app = app.test_client()
+
+    ###############
+    #### tests ####
+    ###############
+
+    def test_home_page(self):
+        response = self.app.get('/', follow_redirects=True)
+        self.assertEqual(response.status_code, 200)
+    
+    def test_animals_page(self):
+        response = self.app.get('/animals', follow_redirects=True)
+        self.assertEqual(response.status_code, 200)
+    
+    def test_organizations_page(self):
+        response = self.app.get('/organizations', follow_redirects=True)
+        self.assertEqual(response.status_code, 200)
+
+if __name__ == "__main__":
+    unittest.main()
